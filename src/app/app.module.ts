fdb14b6330539bb2040fbea70f8ee0ea619c59e3
Liliana Joseline Del Angel Gomez <lilayos000@gmail.com>
2023-01-18 17:46:00 -0600

    Componenetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './Components/lista/lista.component';
+import { ToolbarComponent } from './Components/toolbar/toolbar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    ToolbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
